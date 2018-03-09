c6ac433ec7a21b593e80f1fd561d9f26edefc46c
Dee Reddy <Deesus@users.noreply.github.com>
2018-03-09 12:50:31 -0500

    Refactor: Add comments.

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,13 +1,31 @@
 'use strict';
 
-// Adds CSS property directly to <style> tag:
-function addStylesToDOM (selectorName, attributes) {
-    var output = '';
+  /// -----------------------------------------------------------------------
+  /// Adds CSS a property directly to <style> tag
+  /// This is useful when you need to dynamically compute a CSS value (e.g. branding color) and modify a CSS class accordingly.
+  /// 
+  /// Args:
+  ///   - selector {string}: CSS selector name
+  ///   - cssAttributes {object}: an object literal of attributes and values
+  ///
+  /// Example:
+  ///   addStylesToDOM('#main', {color: 'blue', 'border-left': '1px solid gray'});
+  function addStylesToDOM (selector,
+                           cssAttributes)
+  {
+    var outputString = '';
+    var headElement  = document.getElementsByTagName('head')[0];
 
-    for(var x in attributes) {
-        if (attributes.hasOwnProperty(x)) {
-            output += x + ':' + attributes[x] + ';';
-        }
+    // loop through css properties and 
+    for (var attribute in cssAttributes)
+    {
+      if (cssAttributes.hasOwnProperty(attribute))
+      {
+        outputString += attribute + ':' + cssAttributes[attribute] + ';';
+      }
     }
-    document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', '<style>' + selectorName + '{' + output + '}' + '</style>');
-}
+
+    // append the string before the end of the <head> tag:
+    headElement.insertAdjacentHTML('beforeend',
+                                   '<style>' + selector + '{' + output + '}' + '</style>');
+  }
