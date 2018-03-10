64cff16c95a400f0e24b19fb03afecbc03a5237d
deesus <d.reddy@yandex.com>
2018-03-10 04:59:17 -0500

    Refactor: Covert script to ES6.

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,31 +1,29 @@
 'use strict';
 
-  /// -----------------------------------------------------------------------
-  /// Adds CSS a property directly to <style> tag
-  /// This is useful when you need to dynamically compute a CSS value (e.g. branding color) and modify a CSS class accordingly.
-  /// 
-  /// Args:
-  ///   - selector {string}: CSS selector name
-  ///   - cssAttributes {object}: an object literal of attributes and values
-  ///
-  /// Example:
-  ///   addStylesToDOM('#main', {color: 'blue', 'border-left': '1px solid gray'});
-  function addStylesToDOM (selector,
-                           cssAttributes)
-  {
-    var outputString = '';
-    var headElement  = document.getElementsByTagName('head')[0];
+/// -----------------------------------------------------------------------
+/// Adds CSS a property directly to <style> tag
+/// This is useful when you need to dynamically compute a CSS value (e.g. branding color) and modify a CSS class accordingly.
+/// 
+/// Args:
+///   - selector {string}: CSS selector name
+///   - cssAttributes {object}: an object literal of attributes and values
+///
+/// Example:
+///   addStylesToDOM('#main', {color: 'blue', 'border-left': '1px solid gray'});
+///
+function addStylesToDOM (selector,
+                         cssAttributes) {
+    let outputString = '';
+    let headElement  = document.getElementsByTagName('head')[0];
 
-    // loop through css properties and 
-    for (var attribute in cssAttributes)
-    {
-      if (cssAttributes.hasOwnProperty(attribute))
-      {
-        outputString += attribute + ':' + cssAttributes[attribute] + ';';
-      }
+    // loop through CSS properties:
+    for (let attribute in cssAttributes) {
+        if (cssAttributes.hasOwnProperty(attribute)) {
+            outputString += `${attribute}:${cssAttributes[attribute]};`;
+        }
     }
 
     // append the string before the end of the <head> tag:
     headElement.insertAdjacentHTML('beforeend',
-                                   '<style>' + selector + '{' + output + '}' + '</style>');
-  }
+                                   `<style>${selector}{${outputString}}</style>`);
+}
